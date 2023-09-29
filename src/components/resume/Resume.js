b7b4db8183a4f3571be898b6a584dd119fa466a8
arunvemireddy <vemireddyarun0701@gmail.com>
2023-09-28 23:49:10 -0600

    Automated commit from Jenkins

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,5 +1,53 @@
 import React from "react";
-import ResumeItem from "./ResumeItem";
+import { formatDescription } from "../../utils/formating";
+import "./Resume.css";
+
+const ResumeItem = (props) => {
+  const { item } = props;
+
+  let companyClass = "";
+  switch (item.companyName) {
+    case "Virtusa":
+      companyClass = "virtusaBg";
+      break;
+    case "Utah State University":
+      companyClass = "usulogo";
+      break;
+    case "SICET":
+      companyClass = "sicetlogo";
+      break;
+    default:
+      break;
+  }
+
+  const formattedDescription = formatDescription(item.description);
+
+  return (
+    <div className="col-twelve">
+      <div className="timeline-wrap">
+        <div
+          className="timeline-block"
+          style={{
+            background: `url('./images/${companyClass}.png')`,
+            backgroundSize: "100%",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="timeline-header">
+            <h3>{item.position}</h3>
+            <p>
+              {item.from} - {item.to}
+            </p>
+          </div>
+          <div className="timeline-content">
+            <h4>{item.companyName}</h4>
+            <p>{formattedDescription}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Resume = (props) => (
   <section id="resume" className="grey-section">
