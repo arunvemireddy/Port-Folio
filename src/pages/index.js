05fe6481da771ca99229b1f86b0ca734b51f0ae0
arunvemireddy <vemireddyarun0701@gmail.com>
2023-10-11 12:45:17 -0600

    Automated commit from Jenkins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Layout from "../components/Layout";
 import { Helmet } from "react-helmet";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,7 +7,23 @@ import '../../static/css/base.css';
 import '../../static/css/vendor.css';
 import '../../static/css/main.css';
 
-// Define URL variables
+// Google Analytics tracking code
+const gaScript = document.createElement('script');
+gaScript.async = true;
+gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-Z8RZFDYV';
+
+// Append the script to the document's head
+document.head.appendChild(gaScript);
+
+gaScript.onload = () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-Z8RZFDYV');
+};
+
 const googleFontsURL = 'https://fonts.googleapis.com/css?family=Merriweather';
 const siteTitle = 'portfolio';
 const helloWorldText = 'Hello, World!';
@@ -26,4 +43,4 @@ const IndexPage = () => {
   );
 };
 
-export default IndexPage;
+ReactDOM.render(<IndexPage />, document.getElementById('root'));
